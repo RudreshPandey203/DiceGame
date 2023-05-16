0428d1420aa456497b1a92ec2d0b8bc8f3e30730
RudreshPandey203 <126448002+RudreshPandey203@users.noreply.github.com>
2023-05-16 15:13:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var a = Math.ceil(Math.random() * 6);
+function play(){var a = Math.ceil(Math.random() * 6);
 var b = Math.ceil(Math.random() * 6);
 var imgsrc = "images/dice"+a+".png";
 var imgsrc2 = "images/dice"+b+".png";
@@ -12,4 +12,11 @@ else if(a<b){
 }
 else {
 document.querySelector("h1").innerHTML="Draw";
+}}
+    document.querySelector(".Restart").addEventListener("click",res);
+    document.querySelector(".Play").addEventListener("click",play);
+function res(){
+    document.querySelector(".img1").setAttribute("src","images/dice6.png");
+document.querySelector(".img2").setAttribute("src","images/dice6.png");
+document.querySelector("h1").innerHTML="DICE GAME";
 }
